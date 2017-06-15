1f42fbd8cbdc62f0e5c8565fa6e7b87903b42229
floresjann <flores-j@cyscorpions.com>
2017-06-15 13:47:05 +0800

    tranfered getComments() fixed indentions

--- app/models/thread.php
+++ app/models/thread.php
@@ -1,59 +1,63 @@
 <?php
 
-    class Thread extends AppModel
-    {
-        public $validation = array(
-            'title' => array(
-                'length' => array(
-                    'validate_between', 1, 30,
-                ),
+class Thread extends AppModel
+{
+    public $validation = array(
+        'title' => array(
+            'length' => array(
+                'validate_between', 1, 30,
             ),
-        );
+        ),
+    );
 
-        public static function getAll()
-        {
-            $threads = array();
-            $db = DB::conn();
-            $rows = $db->rows('SELECT * FROM thread');
-            foreach ($rows as $row){
-                $threads[] = new Thread($row);
-            }
-            return $threads;
+    public static function getAll()
+    {
+        $threads = array();
+        $db = DB::conn();
+        $rows = $db->rows('SELECT * FROM thread');
+        foreach ($rows as $row) {
+            $threads[] = new Thread($row);
         }
-
-        public static function get($id)
-        {
-            $db = DB::conn();
-            $row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
-            return new self($row);
-        }
-
-        public function write(Comment $comment)
-        {
-            $db = DB::conn();
-            $params = array(
-                'thread_id' => $this->id,
-                'username' => $comment->username,
-                'body' => $comment->body,
-                'created' => date('Y-m-d H:i:s')
-            );
-            $db->insert('comment', $params);
-        }
-
-        public function create(Comment $comment)
-        {
-            $this->validate();
-            $comment->validate();
-            if ($this->hasError() || $comment->hasError()) {
-                throw new ValidationException('invalid thread or comment');
-            }
-            $db = DB::conn();
-            $db->begin();
-            $db->query('INSERT INTO thread SET title = ?, created = NOW()', array($this->title));
-               $this->id = $db->lastInsertId();
-            $this->write($comment);
-            $db->commit();
-        }
-
-
+        return $threads;
     }
+
+    public static function get($id)
+    {
+        $db = DB::conn();
+        $row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
+        return new self($row);
+    }
+
+    public function getComments()
+    {
+        return Comment::getByThreadId($this->id);
+    }
+
+    public function write(Comment $comment)
+    {
+        $db = DB::conn();
+        $params = array(
+            'thread_id' => $this->id,
+            'username' => $comment->username,
+            'body' => $comment->body,
+            'created' => date('Y-m-d H:i:s')
+        );
+        $db->insert('comment', $params);
+    }
+
+    public function create(Comment $comment)
+    {
+        $this->validate();
+        $comment->validate();
+        if ($this->hasError() || $comment->hasError()) {
+            throw new ValidationException('invalid thread or comment');
+        }
+        $db = DB::conn();
+        $db->begin();
+        $db->query('INSERT INTO thread SET title = ?, created = NOW()', array($this->title));
+        $this->id = $db->lastInsertId();
+        $this->write($comment);
+        $db->commit();
+    }
+
+}

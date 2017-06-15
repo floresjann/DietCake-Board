af3ab2ca70c8b1338bc63826a26fc4289e3db5c9
floresjann <flores-j@cyscorpions.com>
2017-06-15 13:43:21 +0800

    fixed indentions

--- app/models/comment.php
+++ app/models/comment.php
@@ -1,41 +1,37 @@
 <?php
-    class Comment extends AppModel
+
+class Comment extends AppModel
+{
+
+    public $validation = array(
+        'username' => array(
+            'length' => array(
+                'validate_between', 1, 16,
+            ),
+        ),
+        'body' => array(
+            'length' => array(
+                'validate_between', 1, 200,
+            ),
+        ),
+    );
+
+    public function write(Comment $comment)
     {
-
-        public $validation = array(
-            'username' => array(
-                'length' => array(
-                    'validate_between', 1, 16,
-                ),
-            ),
-            'body' => array(
-                'length' => array(
-                    'validate_between', 1, 200,
-                ),
-            ),
-        );
-
-        public function write(Comment $comment)
-        {
-            if (!$comment->validate()) {
-                throw new ValidationException('invalid comment');
-            }
+        if (!$comment->validate()) {
+            throw new ValidationException('invalid comment');
         }
-
-        public function getComments()
-        {
-            return Comment::getByThreadId($this->id);
-        }
-
-        public static function getByThreadId($thread_id)
-        {
-            $comments = array();
-            $db = DB::conn();
-            $rows = $db->rows('SELECT * FROM comment WHERE thread_id = ?', array($this->id));
-            foreach ($rows as $row) {
-                $comments[] = new self($row);
-            }
-            return $comments;
-        }
-
     }
+
+    public static function getByThreadId($thread_id)
+    {
+        $comments = array();
+        $db = DB::conn();
+        $rows = $db->rows('SELECT * FROM comment WHERE thread_id = ?', array($this->id));
+        foreach ($rows as $row) {
+            $comments[] = new self($row);
+        }
+        return $comments;
+    }
+
+}
